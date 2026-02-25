b6c49c6eb88e7a0a91b23356d5305119a4fbb88e
Nadia Devi Jayapragash <jayapnad@students.zhaw.ch>
2026-02-25 11:14:42 +0100

    Adresse

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
--Nadia Devi Jayapragash (jayapnad@students.zhaw.ch)
--Giuliana Gross (grossgiu@students.zhaw.ch)
+- Nadia Devi Jayapragash (jayapnad@students.zhaw.ch)
+- Giuliana Gross (grossgiu@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
